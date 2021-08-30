5d130f3fbf8c9fb03130d84c3958327c43255bad
Me-to <49270515+Me-to@users.noreply.github.com>
2021-08-30 17:05:54 +0800

    Update MallProductApplicationTests.java

--- mall-product/src/test/java/com/qian/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/qian/mall/product/MallProductApplicationTests.java
@@ -40,10 +40,10 @@ class MallProductApplicationTests {
 //    @Test
 //    public void upload() throws IOException {
 ////// Endpoint以杭州为例，其它Region请按实际情况填写。
-////        String endpoint = "https://oss-cn-zhangjiakou.aliyuncs.com";
+////        String endpoint = "####";
 ////// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录RAM控制台创建RAM账号。
-////        String accessKeyId = "LTAI4GEPT8NWiyhQ3XHtbWRG";
-////        String accessKeySecret = "o7gDXT6RfY8H9udojWZviW4ySGYbaI";
+////        String accessKeyId = "####";
+////        String accessKeySecret = "###";
 ////      String simpleDateFormat=new SimpleDateFormat("yyy-MM-dd").format(new Date());
 ////        String bucketName = "qian-mall";
 ////// <yourObjectName>上传文件到OSS时需要指定包含文件后缀在内的完整路径，例如abc/efg/123.jpg。
